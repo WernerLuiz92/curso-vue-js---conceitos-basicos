211b3af965ff7ab21d2217d4f5075dc5d1b517e9
WernerLuiz92 <68384543+WernerLuiz92@users.noreply.github.com>
2022-12-30 13:41:25 -0300

    feat(Directives): Add a new file with a to-do-list to implement the directives

--- src/App.vue
+++ src/App.vue
@@ -12,10 +12,14 @@
     "
   >
     <img alt="Vue logo" src="https://vuejs.org/images/logo.png" />
-    <DataBinding />
+    <div class="space-y-5">
+      <DataBinding />
+      <Directives />
+    </div>
   </div>
 </template>
 
 <script setup>
 import DataBinding from './components/DataBinding.vue';
+import Directives from './components/Directives.vue';
 </script>
---
+++ src/components/Directives.vue
@@ -0,0 +1,36 @@
+<template>
+  <h1 class="text-3xl font-bold">Minha Lista</h1>
+  <ul class="mt-5">
+    <li
+      @click="task.isDone = !task.isDone"
+      :class="['flex items-center', task.isDone ? 'line-through' : '']"
+      v-for="(task, index) in tasks"
+      :key="`${task.name}-${index}`"
+    >
+      <CheckIcon class="w-5 h-5" v-if="task.isDone" />
+      <XMarkIcon class="w-5 h-5" v-else />
+      {{ task.name }}
+    </li>
+  </ul>
+</template>
+
+<script>
+import { CheckIcon, XMarkIcon } from '@heroicons/vue/20/solid';
+export default {
+  name: 'Directives',
+
+  components: {
+    CheckIcon,
+    XMarkIcon,
+  },
+
+  data() {
+    return {
+      tasks: [
+        { name: 'Fazer o curso', isDone: false },
+        { name: 'Almoçar', isDone: false },
+      ],
+    };
+  },
+};
+</script>
